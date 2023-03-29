5185f267341e0a035a5cc9e1c7e613009f116abb
st61014 <st61014@upce.cz>
2023-03-29 16:15:19 +0200

    import Material UI, experimenting with it

--- cv07/src/page/Tasks.tsx
+++ cv07/src/page/Tasks.tsx
@@ -1,24 +1,39 @@
 import TaskCard from "../component/TaskCard";
 import {Task, tasks} from "../data/init-data";
-import {useState} from "react";
+import React, {useState} from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 const Tasks = () => {
     const [taskList, setTaskList] = useState<Array<Task>>(tasks);
     const taskDoneHandle = (task: Task) =>{
         setTaskList([...taskList]);
     }
-    return <div>
-        <h1>Aktuální tasky</h1>
+    return <Container  maxWidth="sm">
+        <Typography
+            component="h1"
+            variant="h3"
+            align="center"
+            color="text.primary"
+            gutterBottom
+        >Aktuální tasky</Typography>
 
         {taskList.filter(t => !t.done).map(t =>
             <TaskCard key={t.id} task={t} onTaskDone={taskDoneHandle}/>)}
 
-        <h1>Splněné tasky</h1>
+        <Typography
+            component="h1"
+            variant="h3"
+            align="center"
+            color="text.primary"
+            gutterBottom
+        >Splněné tasky</Typography>
 
         {taskList.filter(t => t.done).map(t =>
             <TaskCard key={t.id} task={t} onTaskDone={taskDoneHandle}/>)}
 
-    </div>
+        </Container>
 }
 
 export default Tasks;
